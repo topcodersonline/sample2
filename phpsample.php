06c92dc1a20ff881bc5d7f73b4fc465eff8f5151
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2023-03-09 10:36:53 +0100

    Update phpsample.php

--- phpsample.php
+++ phpsample.php
@@ -13,3 +13,19 @@ $target = $_GET["target"];
 header("Location: " . $target);
 
 echo $_GET["user"];
+
+
+$conn = new mysqli("127.0.0.1", "user", "MySecretPassword", "db");
+
+function doSqlQuery() {
+  $query = "SELECT * FROM users WHERE login = '" . $_POST["login"];
+
+  return mysqli_query($conn, $query);
+}
+
+$target = $_GET["target"];
+
+header("Location: " . $target);
+
+echo $_GET["user"];
+
